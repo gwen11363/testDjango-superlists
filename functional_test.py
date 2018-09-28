fc829b15a6e9d5158be2a477ea5aeafaf488a0bf
kate_su <Kate_Su@compal.com>
2018-09-28 11:21:03 +0800

    First FT, using comment to explain the spec and also use unittest.

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-#browser.get("http://railway.hinet.net/Foreign/TW/etno_roundtrip.html")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #發現一個很酷的線上待辦事項app
+        #察看首頁
+        self.browser.get('http://localhost:8000')
+
+        #發現網頁標題與標頭顯示待辦事項清單
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        #馬上輸入一個待辦事項
+
+        #在文字方塊輸入"購買孔雀羽毛"
+
+        #按下enter時，網頁會更新，現在網頁列出
+        #"1.購買孔雀羽毛"，一個待辦事項清單項目
+
+        #此時仍有一個文字方塊，讓使用者可以輸入另一個項目
+        #輸入"使用孔雀羽毛製作蒼蠅"
+
+        #網頁再次更新，現在清單中有兩個代辦項目
+
+        #網頁產生一個唯一的URL給使用者
+        #網頁有一些文字說明這個效果
+
+        #前往URL-顯示使用者的代辦清單
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
