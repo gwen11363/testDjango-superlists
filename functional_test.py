0ba12c4bff90a639ecca640f72a516cc15c7f2ed
kate_su <Kate_Su@compal.com>
2018-09-28 15:00:16 +0800

    Front page HTML now generated from a tamplate

--- functional_test.py
+++ functional_test.py
@@ -37,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         #此時仍有一個文字方塊，讓使用者可以輸入另一個項目
